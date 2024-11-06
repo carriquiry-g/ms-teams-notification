5ce11f3c059aceed47dc044cc6787185842aaa8d
Gastón Carriquiry <carriquiry@ort.edu.uy>
2024-11-06 16:56:45 -0300

    feat(main): change input name

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,7 @@ import {Octokit} from '@octokit/rest'
 import axios from 'axios'
 import moment from 'moment-timezone'
 import {createMessageCard} from './message-card'
-
-const escapeMarkdownTokens = (text: string) =>
-  text
-    .replace(/\n\ {1,}/g, '\n ')
-    .replace(/\_/g, '\\_')
-    .replace(/\*/g, '\\*')
-    .replace(/\|/g, '\\|')
-    .replace(/#/g, '\\#')
-    .replace(/-/g, '\\-')
-    .replace(/>/g, '\\>')
+import {CardConfig, NotificationStyle} from './types'
 
 async function run(): Promise<void> {
   try {
@@ -23,17 +14,19 @@ async function run(): Promise<void> {
 
     const notificationSummary =
       core.getInput('notification-summary') || 'GitHub Action Notification'
-    const notificationColor = core.getInput('notification-color') || '0b93ff'
+    const notificationStyle =
+      (core.getInput('notification-style') as NotificationStyle) || 'accent'
     const timezone = core.getInput('timezone') || 'UTC'
     const verboseLogging = core.getInput('verbose-logging') == 'true'
     const timestamp = moment()
       .tz(timezone)
       .format('dddd, MMMM Do YYYY, h:mm:ss a z')
 
-    const [owner, repo] = (process.env.GITHUB_REPOSITORY || '').split('/')
-    const sha = process.env.GITHUB_SHA || ''
-    const runId = process.env.GITHUB_RUN_ID || ''
-    const runNum = process.env.GITHUB_RUN_NUMBER || ''
+    const [owner, repo] = (process.env.GITHUB_REPOSITORY ?? '').split('/')
+    const sha = process.env.GITHUB_SHA ?? ''
+    const workflowName = process.env.GITHUB_WORKFLOW ?? 'CI'
+    const runId = process.env.GITHUB_RUN_ID ?? ''
+    const runNum = process.env.GITHUB_RUN_NUMBER ?? ''
     const params = {owner, repo, ref: sha}
     const repoName = params.owner + '/' + params.repo
     const repoUrl = `https://github.com/${repoName}`
@@ -42,18 +35,33 @@ async function run(): Promise<void> {
     const commit = await octokit.repos.getCommit(params)
     const author = commit.data.author
 
-    const messageCard = await createMessageCard(
+    const cardConfig: CardConfig = {
       notificationSummary,
-      notificationColor,
-      commit,
-      author,
-      runNum,
-      runId,
-      repoName,
-      sha,
-      repoUrl,
-      timestamp
-    )
+      notificationStyle,
+      timestamp,
+      workflow: {
+        name: workflowName,
+        runNumber: runNum,
+        runId,
+        repo: {
+          name: repoName,
+          url: repoUrl
+        },
+        commit: {
+          sha,
+          data: commit.data
+        },
+        author: {
+          name: author?.name,
+          avatarUrl: author?.avatar_url,
+          login: author?.login,
+          htmlUrl: author?.html_url,
+          url: author?.url
+        }
+      }
+    }
+
+    const messageCard = await createMessageCard(cardConfig)
 
     if (verboseLogging) {
       console.log(messageCard)
