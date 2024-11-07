702555a1a7e24c5c5c84df47de6d6634621302cd
Gastón Carriquiry <carriquiry@ort.edu.uy>
2024-11-07 14:21:26 -0300

    fix(card): fix author name logic

--- src/message-card.ts
+++ src/message-card.ts
@@ -71,7 +71,7 @@ export function createMessageCard(cardConfig: CardConfig): any {
                       },
                       {
                         type: 'TextBlock',
-                        text: `by ${author_url} on ${cardConfig.timestamp}`,
+                        text: `by ${author.name ? author.name + ' (' + author_url + ') ' : author_url} on ${cardConfig.timestamp}`,
                         isSubtle: true,
                         wrap: true
                       }
